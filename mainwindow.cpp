747ccae6cee8d8eb14484332db41c257236d587e
skawu <maoxudong0813@163.com>
2019-12-12 19:07:35 +0800

    完成控件的基本功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,106 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDir>
+#include <QFileDialog>
+#include <QDateTime>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->ui->comboBox_stage->addItem(tr(" "));
+    this->ui->comboBox_stage->addItem("pull");
+    this->ui->comboBox_stage->addItem("build");
+    this->ui->comboBox_stage->addItem("stage");
+    this->ui->comboBox_stage->addItem("prime");
+    this->ui->comboBox_stage->addItem("snap");
+    this->ui->comboBox_stage->addItem("clean");
+
+    exec_shell = new QProcess(this);
+#ifdef Q_OS_WIN32
+    QString program = "C:/Windows/SysWOW64/cmd.exe";
+#endif
+#ifdef Q_OS_UNIX
+    QString program =  "/bin/sh";
+#endif
+    exec_shell->setProgram(program);
+    exec_shell->start();
+
+    connect(exec_shell, SIGNAL(readyReadStandardOutput()), this, SLOT(readProcess()));
+    connect(exec_shell, SIGNAL(finished(int)), this, SLOT(finishedProcess()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_action_opendir_triggered()
+{
+    QDir dir;
+
+    PathName = QFileDialog::getExistingDirectory(this, tr("选择目录"),
+                                                 "E:/Myworkspace/data/",
+                                                 QFileDialog::ShowDirsOnly
+                                                 | QFileDialog::DontResolveSymlinks);
+
+    PathName.replace("/","\\"); //单斜杠转换双斜杠,方便后续存储bin文件
+    ui->label_selectDir->setText(PathName);    //文件名称显示
+
+    dir.cd(PathName);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QDir dir;
+
+    PathName = QFileDialog::getExistingDirectory(this, tr("选择目录"),
+                                                 "E:/Myworkspace/data/",
+                                                 QFileDialog::ShowDirsOnly
+                                                 | QFileDialog::DontResolveSymlinks);
+
+    PathName.replace("/","\\"); //单斜杠转换双斜杠,方便后续存储bin文件
+    ui->label_selectDir->setText(PathName);    //文件名称显示
+
+    // 控制台切换目录
+    QString cmd = "cd " + PathName + "\n";
+    exec_shell->write(cmd.toLocal8Bit());
+
+    QDir::setCurrent(PathName);
+}
+
+void MainWindow::on_comboBox_stage_currentIndexChanged(const QString &arg1)
+{
+    stage_name = arg1;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    extra_param = this->ui->lineEdit_extraParam->text();
+
+
+    // 拼接命令
+    cmd = "snapcraft " + stage_name + " " + extra_param + "\n";
+    this->ui->label_status_cur_dir->setText(cmd);
+
+    // 执行命令
+    exec_shell->write(cmd.toLocal8Bit());
+}
+
+void MainWindow::readProcess(){
+    QString output = QString::fromLocal8Bit(exec_shell->readAll());
+    shell_output += output;
+    this->ui->textBrowser_2->append(output);
+}
+
+void MainWindow::finishedProcess(){
+    qDebug() << __FUNCTION__ << __LINE__;
+}
+
+void MainWindow::on_pushButton_clear_clicked()
+{
+    this->ui->textBrowser_2->clear();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProcess>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,29 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_action_opendir_triggered();
+
+    void on_pushButton_clicked();
+
+    void on_comboBox_stage_currentIndexChanged(const QString &arg1);
+
+    void on_pushButton_2_clicked();
+    void readProcess();
+    void finishedProcess();
+
+    void on_pushButton_clear_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+public:
+    QProcess *exec_shell;
+    QString PathName;
+    QString stage_name;
+    QString extra_param;
+    QString cmd;
+    QString shell_output;
 };
 
 #endif // MAINWINDOW_H
